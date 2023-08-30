64a10c1dddd14288454653294349b5d4e077bbfe
Shubham <samxn81@gmail.com>
2023-08-30 16:05:56 +0530

    base

--- tnt-frontend/src/App.js
+++ tnt-frontend/src/App.js
@@ -14,19 +14,19 @@ import { NavbarProvider } from "./context/NavbarContext";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/Tourism-managment-system">
         <NavbarProvider>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Login />} />
-          <Route exact path="/signup" element={<AuthForm />} />
-          <Route path="/home" exact element={<Home />} />
-          <Route path="/Destinations" element={<Destinations />} />
-          <Route path="/Book" element={<Book />} />
-          <Route exact path="/Contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/Er/:loc" element={<ExternalRedirect />} />
-        </Routes>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/signup" element={<AuthForm />} />
+            <Route path="/home" exact element={<Home />} />
+            <Route path="/Destinations" element={<Destinations />} />
+            <Route path="/Book" element={<Book />} />
+            <Route exact path="/Contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/Er/:loc" element={<ExternalRedirect />} />
+          </Routes>
         </NavbarProvider>
       </Router>
     </div>
