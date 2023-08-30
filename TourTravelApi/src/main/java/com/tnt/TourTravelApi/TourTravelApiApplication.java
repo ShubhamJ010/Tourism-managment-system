077a4abf8805a2a307b6c58ef9d1b8b72e838a90
Shubham <samxn81@gmail.com>
2023-08-30 18:33:10 +0530

    api init

---
+++ TourTravelApi/src/main/java/com/tnt/TourTravelApi/TourTravelApiApplication.java
@@ -0,0 +1,13 @@
+package com.tnt.TourTravelApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourTravelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourTravelApiApplication.class, args);
+	}
+
+}
