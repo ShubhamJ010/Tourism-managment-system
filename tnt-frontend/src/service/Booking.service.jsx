de701c0de2564f40989eeadc5fa83b4f103e6acd
Shubham <samxn81@gmail.com>
2023-09-02 12:45:21 +0530

    helper

--- tnt-frontend/src/service/Booking.service.jsx
+++ tnt-frontend/src/service/Booking.service.jsx
@@ -1,10 +1,9 @@
 import { myAxios } from "./helper";
 
-const API_BASE_URL = "http://localhost:8083";
 const BookingService = {
   addbooking: (bookingData) => {
     return myAxios
-      .post(`http://localhost:8083/booking/addbooking`, bookingData)
+      .post(`/booking/addbooking`, bookingData)
       .then((response) => response.data)
       .catch((error) => {
         throw error;
